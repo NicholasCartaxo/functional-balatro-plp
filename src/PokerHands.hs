c6f65b2bb20e9cd6fa43cf9d355383bbcd59589a
Nicholas Cartaxo <nicholas.cartaxo.de.souza.rangel.alves@ccc.ufcg.edu.br>
2025-12-26 06:16:07 -0300

    added stack project usage

---
+++ src/PokerHands.hs
@@ -0,0 +1,83 @@
+module PokerHands
+  ( PokerHand(..)
+
+  ) where
+import Cards
+import Data.List (sort,find)
+import Data.Maybe (isJust,fromJust)
+
+data PokerHand = StraightFlush | FourOfAKind | FullHouse | Flush | Straight | ThreeOfAKind | TwoPair | Pair | HighCard
+  deriving (Eq, Show, Ord, Enum, Bounded)
+
+getStraight :: [Card] -> [Card]
+getStraight xs
+  |length xs /= 5 = []
+  |otherwise = if straightLoop (sort xs) then xs else []
+  where
+    straightLoop [] = True
+    straightLoop [_] = True
+    straightLoop [Card R5 _, Card RA _] = True
+    straightLoop (Card x _:Card y __:xs) =
+      (y == succ x) && straightLoop (Card y __:xs)
+
+getFlush :: [Card] -> [Card]
+getFlush xs
+  |length xs /= 5 = []
+  |otherwise = if flushLoop xs then xs else []
+  where
+    flushLoop [] = True
+    flushLoop [_] = True
+    flushLoop (Card _ x:Card __ y:xs) =
+      (x == y) && flushLoop (Card __ y:xs)
+
+countOccurrences :: [Card] -> [(Rank,Int)]
+countOccurrences xs = [(i,j) | i <- [minBound..maxBound], j <- [length (filter (\(Card x _)-> x==i) xs)], j /= 0]
+
+getNRepeats :: Int -> [Card] -> [Card]
+getNRepeats n xs = filter isFromRank xs
+  where
+    occurrences = countOccurrences xs
+    isFromRank (Card rank _) = Just rank == fmap fst (find (\(_,times) -> times == n) occurrences)
+
+getFour :: [Card] -> [Card]
+getFour = getNRepeats 4
+
+getFullHouse :: [Card] -> [Card]
+getFullHouse xs = filter isInRanks xs
+  where
+    occurrences = countOccurrences xs
+    rankThree = fmap fst (find (\(_,times) -> times == 3) occurrences)
+    rankTwo = fmap fst (find (\(_,times) -> times == 2) occurrences)
+    isInRanks (Card rank _) = isJust rankThree && isJust rankTwo && elem (Just rank) [rankThree, rankTwo]
+
+getThree :: [Card] -> [Card]
+getThree = getNRepeats 3
+
+getTwoPair :: [Card] -> [Card]
+getTwoPair xs = filter isInRanks xs
+  where
+    occurrences = countOccurrences xs
+    ranksTwo = map fst (filter (\(_,times) -> times == 2) occurrences)
+    isInRanks (Card rank _) = length ranksTwo == 2 && elem rank ranksTwo
+
+getPair :: [Card] -> [Card]
+getPair = getNRepeats 2
+
+getHigh :: [Card] -> [Card]
+getHigh xs = [maximum xs]
+
+getFuncOfHand :: PokerHand -> [Card] -> [Card]
+getFuncOfHand StraightFlush xs
+  |(not . null) (getStraight xs) && (not . null) (getFlush xs) = xs
+  |otherwise = []
+getFuncOfHand FourOfAKind xs = getFour xs
+getFuncOfHand FullHouse xs = getFullHouse xs
+getFuncOfHand Flush xs = getFlush xs
+getFuncOfHand Straight xs = getStraight xs
+getFuncOfHand ThreeOfAKind xs = getThree xs
+getFuncOfHand TwoPair xs = getTwoPair xs
+getFuncOfHand Pair xs = getPair xs
+getFuncOfHand HighCard xs = getHigh xs
+
+getPokerHandAndCards :: [Card] -> (PokerHand, [Card])
+getPokerHandAndCards xs = fromJust (find (not . null . snd) (map (\x -> (x,getFuncOfHand x xs)) [minBound..maxBound]))
--- src/cards.hs
+++ src/cards.hs
@@ -1,6 +1,9 @@
-module Cards where
-import Data.List
-import Data.Maybe
+module Cards
+  ( Rank(..)
+  , rankValue
+  , Suit(..)
+  , Card(..)
+  ) where
 
 data Rank = R2 | R3 | R4 | R5 | R6 | R7 | R8 | R9 | R10 | RJ | RQ | RK | RA
   deriving (Eq, Ord, Enum, Bounded)
@@ -35,77 +38,3 @@ instance Show Suit where
 
 data Card = Card Rank Suit
   deriving (Eq, Show, Ord)
-
-data PokerHand = StraightFlush | FourOfAKind | FullHouse | Flush | Straight | ThreeOfAKind | TwoPair | Pair | HighCard
-  deriving (Eq, Show, Ord, Enum, Bounded)
-
-getStraight :: [Card] -> [Card]
-getStraight xs
-  |length xs /= 5 = []
-  |otherwise = if straightLoop (sort xs) then xs else []
-  where
-    straightLoop [x] = True
-    straightLoop [Card R5 _, Card RA _] = True
-    straightLoop (Card x _:Card y __:xs) =
-      (y == succ x) && straightLoop (Card y __:xs)
-
-getFlush :: [Card] -> [Card]
-getFlush xs
-  |length xs /= 5 = []
-  |otherwise = if flushLoop xs then xs else []
-  where
-    flushLoop [x] = True
-    flushLoop (Card _ x:Card __ y:xs) =
-      (x == y) && flushLoop (Card __ y:xs)
-
-countOccurrences :: [Card] -> [(Rank,Int)]
-countOccurrences xs = [(i,j) | i <- [minBound..maxBound], j <- [length (filter (\(Card x _)-> x==i) xs)], j /= 0]
-
-getNRepeats :: Int -> [Card] -> [Card]
-getNRepeats n xs = filter isFromRank xs
-  where
-    occurrences = countOccurrences xs
-    isFromRank (Card rank _) = Just rank == fmap fst (find (\(_,times) -> times == n) occurrences)
-
-getFour :: [Card] -> [Card]
-getFour = getNRepeats 4
-
-getFullHouse :: [Card] -> [Card]
-getFullHouse xs = filter isInRanks xs
-  where
-    occurrences = countOccurrences xs
-    rankThree = fmap fst (find (\(_,times) -> times == 3) occurrences)
-    rankTwo = fmap fst (find (\(_,times) -> times == 2) occurrences)
-    isInRanks (Card rank _) = isJust rankThree && isJust rankTwo && elem (Just rank) [rankThree, rankTwo]
-
-getThree :: [Card] -> [Card]
-getThree = getNRepeats 3
-
-getTwoPair :: [Card] -> [Card]
-getTwoPair xs = filter isInRanks xs
-  where
-    occurrences = countOccurrences xs
-    ranksTwo = map fst (filter (\(_,times) -> times == 2) occurrences)
-    isInRanks (Card rank _) = length ranksTwo == 2 && elem rank ranksTwo
-
-getPair :: [Card] -> [Card]
-getPair = getNRepeats 2
-
-getHigh :: [Card] -> [Card]
-getHigh xs = [maximum xs]
-
-getFuncOfHand :: PokerHand -> [Card] -> [Card]
-getFuncOfHand StraightFlush xs
-  |(not . null) (getStraight xs) && (not . null) (getFlush xs) = xs
-  |otherwise = []
-getFuncOfHand FourOfAKind xs = getFour xs
-getFuncOfHand FullHouse xs = getFullHouse xs
-getFuncOfHand Flush xs = getFlush xs
-getFuncOfHand Straight xs = getStraight xs
-getFuncOfHand ThreeOfAKind xs = getThree xs
-getFuncOfHand TwoPair xs = getTwoPair xs
-getFuncOfHand Pair xs = getPair xs
-getFuncOfHand HighCard xs = getHigh xs
-
-getPokerHandAndCards :: [Card] -> (PokerHand, [Card])
-getPokerHandAndCards xs = fromJust (find (not . null . snd) (map (\x -> (x,getFuncOfHand x xs)) [minBound..maxBound]))
